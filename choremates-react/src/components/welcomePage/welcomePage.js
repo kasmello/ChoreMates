f69eb891fe7ef48a8e478844052501d7f6519e7f
kasmello <karmel.gandahusada@gmail.com>
2025-01-01 19:06:36 +0800

    styling and centering of react

--- choremates-react/src/components/welcomePage/welcomePage.js
+++ choremates-react/src/components/welcomePage/welcomePage.js
@@ -7,21 +7,21 @@ import './welcomePage.css';
 const welcomePage = () => {
    return (
       <div>
-         <div>ChoreMates</div>
-         <div>UserName: <input
+         <div style={{ textAlign: 'center' }}><h1>ChoreMates</h1></div>
+         <div className='credentials'>UserName: <input
             type="text"
             placeholder="Type something here"
          />
          </div>
-         <div>Password: <input
+         <div className='credentials'>Password: <input
             type="text"
             placeholder="Type something here"
          /></div>
-         <div>Remember Me <input
+         <div style={{ textAlign: 'center' }}>Remember Me <input
           type="checkbox"
          /></div>
-         <div><button>Login</button></div>
-         <div>Don't have an account? Register</div>
+         <div style={{ textAlign: 'center' }}><button>Login</button></div>
+         <div style={{ textAlign: 'center' }}>Don't have an account? Register</div>
       </div>
       
    );
