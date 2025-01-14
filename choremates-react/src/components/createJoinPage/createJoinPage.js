393f16d1d6f43f0341e6d6c76b008895cfcf01d1
kasmello <karmel.gandahusada@gmail.com>
2025-01-15 04:57:59 +0800

    removed 'who can join' for scope reasons

--- choremates-react/src/components/createJoinPage/createJoinPage.js
+++ choremates-react/src/components/createJoinPage/createJoinPage.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import './createJoinPage.css';
 import './createJoinPage.js'
 
 
 const createJoinPage = () => {
+
+
+
    return (
       <div>
          <div className='title'><h1>Create a Household</h1></div>
@@ -18,38 +21,20 @@ const createJoinPage = () => {
             type="text"
             placeholder="Type something here"
          /></div>
-         <div>Display Picture: <button>lol</button>
-         </div>
-         <div>Who can join: <div>
-         <label>
-          <input
-            type="radio"
-            value="A"
-            // checked={selectedOption === "A"}
-            // onChange={handleChange}
-          />
-          Anyone with the code
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="B"
-            // checked={selectedOption === "B"}
-            // onChange={handleChange}
-          />
-          Invite only
-        </label>
-         </div>
+         <div><button>Upload Display Picture</button>
          </div>
          
-
          </div>
-         <div className='join'>OR enter a household code to join</div>
-         <input
-            type="text"
-            placeholder="Type something here"
-         />
-         <button>Join</button>
+         <div className='join'>
+            OR enter a household code to join
+            <div>
+               <input
+                  type="text"
+                  placeholder="Type something here"
+               />
+               <button>Join</button>
+            </div>
+         </div>
       </div>
       
    );
